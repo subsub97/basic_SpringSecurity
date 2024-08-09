7d73aeb5281f142871f5255c354b06a7374a946b
Ho-Da-Dak <godqhr721@gmail.com>
2024-08-10 01:35:52 +0900

    init Spring Security learning Project

---
+++ src/test/java/org/example/tubesecurity/TubeSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.tubesecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TubeSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
